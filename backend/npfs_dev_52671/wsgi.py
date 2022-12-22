8bf42d2571be2cc5ff80c51e2a900425e2cf4e98
Crowdbotics <engineering@crowdbotics.com>
2022-12-22 13:02:37 +0000

    Initial commit.

---
+++ backend/npfs_dev_52671/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for npfs_dev_52671 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'npfs_dev_52671.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "npfs-dev-52671",
+  url: "https://npfs-dev-52671.botics.co",
+  api: "https://npfs-dev-52671.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
